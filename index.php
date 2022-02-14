eea60f9e61758a09bf0797ba750f73237398c103
Rafa <rgm0002@alu.medac.es>
2022-02-14 19:33:04 +0100

    chat

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $message = $update['message']['text'];
 
 switch($message) {
     case '/start':
-        $response = 'Me has iniciado';
+        $response = 'Me has iniciado, para comenzar pon /help para ver los comandos que puedes utilizar';
         sendMessage($chatId, $response);
         break;
     case '/info':
@@ -28,6 +28,10 @@ switch($message) {
         /help: Te ayuda que comandos puedes poner en este bot';
         sendMessage($chatId, $response);
         break;
+    case '/noticias':
+       
+        sendMessage($chatId);
+        break;
     default:
         $response = 'No te he entendido';
         sendMessage($chatId, $response);
@@ -38,4 +42,28 @@ function sendMessage($chatId, $response) {
     $url = $GLOBALS['website'].'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
     file_get_contents($url);
 }
+
+function Noticias($chatId, $response) {
+    include("simple_html_dom.php");
+    $context=stream_context_create(array('http' => array('header' => "Accept: application/xml")));
+    $url="http://www.europapress.es/rss/rss.aspx";
+
+    $xmlstring= file_get_contents($url, false, $context);
+
+    $xml = simplexml_load_string($xmlstring, "SimpleXMLElement", LIBXML_NOCDATA);
+    $json =json_encode($xml);
+    $array = json_decode($json, TRUE);
+
+    for($i=0; $i<9; $i++){
+        $titulos = $titulos. "\n\n".$array['chanel']['item'][$i]['title']."<a href='".$array['chanel']['item'][$i]['link']."'>
+        +info</a>";
+        
+    }
+
+    sendMessage($chatId, $titulos);
+
+
+
+}
+
 ?>

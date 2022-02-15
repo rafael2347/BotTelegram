06b4b288f1c9a5644aa4084c988e489c5ff00dad
Rafa <rgm0002@alu.medac.es>
2022-02-15 20:19:39 +0100

    chat

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $update = json_decode($input, TRUE);
 $chatType = $update["message"]["chat"]["id"];
 $chatId = $update['message']['chat']['id'];
 $message = $update['message']['text'];
-// $repl = $update['message']['reply_to_message']['text'];
+// $reply = $update['message']['reply_to_message']['text'];
 
 switch($message) {
     case '/start':

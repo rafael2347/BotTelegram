10cf18e0b0d91b781c9eae51ba38db3b61e6f02a
Rafa <rgm0002@alu.medac.es>
2022-02-17 15:35:19 +0100

    chat

--- index.php
+++ index.php
@@ -39,6 +39,7 @@ switch($message) {
          New York Times: /newyork
          EuropaExpress: /europapress
          La Vanguardia: /vanguardia
+         El Pais: /elpais
          ';
         sendMessage($chatId, $response);
         break;
@@ -82,6 +83,9 @@ switch($message) {
         break;
     case '/sportgolf':
         Sportgolf($chatId);
+        break;
+    case '/elpais':
+        Elpais($chatId);
         break;
     default:
         $response = 'No te he entendido';
@@ -327,5 +331,27 @@ function Sportgolf ($chatId) {
 
 }
 
+function Elpais ($chatId) {
+    include("simple_html_dom.php");
+    $context=stream_context_create(array('http' => array('header' => "Accept: application/xml")));
+   
+    $url="https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/portada";
+
+    $xmlstring= file_get_contents($url, false, $context);
+
+    $xml = simplexml_load_string($xmlstring, "SimpleXMLElement", LIBXML_NOCDATA);
+    $json =json_encode($xml);
+    $array = json_decode($json, TRUE);
+
+    for($i=0; $i<9; $i++){
+        $titulos = $titulos. "\n\n".$array['channel']['item'][$i]['title']."<a href='".$array['channel']['item'][$i]['link']."'>
+ Pincha aquí para más información</a>";
+        
+    }
+
+    sendMessage($chatId, $titulos);
+
+
+}
 
 ?>

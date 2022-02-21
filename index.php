962f8608be16b6c081d551cf91d7fcfb3f9ae0f1
Rafa <rgm0002@alu.medac.es>
2022-02-21 16:28:08 +0100

    chat

--- index.php
+++ index.php
@@ -539,7 +539,7 @@ function eltiempo ($chatId) {
     $array = json_decode($json, TRUE);
 
     for($i=0; $i<9; $i++){
-        $titulos = $titulos. "\n\n".$array['channel']['item'][$i]["description"]['title']."<a href='".$array['channel']['item'][$i]['link']."'>
+        $titulos = $titulos. "\n\n".$array['channel']['item'][$i]["instruction"]."<a href='".$array['channel']['item'][$i]['link']."'>
  Pincha aquí para más información</a>";
         
     }
